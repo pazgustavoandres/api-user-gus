d6469482e5abf653e2fc7400519b4b559eea7540
eber-cba <ebercoronel29@gmail.com>
2025-04-07 18:28:05 -0300

    first commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  allowExitOnIdle: true,
+});
+
+pool
+  .connect()
+  .then(() => console.log("✅ Conectado a la base de datos"))
+  .catch((err) => {
+    console.error("❌ Error al conectar con la base de datos:", err.message);
+    process.exit(1);
+  });
+
+module.exports = pool;
