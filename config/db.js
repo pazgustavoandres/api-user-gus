8e3722122515c10946ce67d24b4ba9ced90582d2
Gus <you@example.com>
2025-04-08 17:27:28 -0400

    fix db

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
 const { Pool } = require("pg");
 require("dotenv").config();
-
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  },
   allowExitOnIdle: true,
 });
-
 pool
   .connect()
   .then(() => console.log("✅ Conectado a la base de datos"))
@@ -16,5 +18,4 @@ pool
     console.error("❌ Error al conectar con la base de datos:", err.message);
     process.exit(1);
   });
-
 module.exports = pool;
